3b9fda59743eb1092b673eeb1c40cc8b3fdeebfb
Eana Hufwe <eana@1a23.com>
2026-05-29 15:17:12 -0700

    Test expectation fixes

--- tests/calculator/integration/date-time-literals.integration.test.ts
+++ tests/calculator/integration/date-time-literals.integration.test.ts
@@ -101,7 +101,7 @@ describe("Integration Tests - Date and Time Literals", () => {
       expect(result.results[2].result).toBe("1 h 30 min");
       expect(result.results[3].result).toMatch(/(tomorrow )?05:00 UTC-3:30/);
       expect(result.results[4].result).toMatch(
-        /(-1 day -6 h -30 min|17 h 30 min)/,
+        /(-1 day -[56] h -30 min|17 h 30 min)/,
       );
       expect(result.results[5].result).toMatch(/(tomorrow )?05:00 UTC\+3:30/);
     });
